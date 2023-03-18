02b36199137a32e28e41d1055bf8bae2c00b91e8
Cremona <felippe.mendes@qintess.com>
2023-03-18 17:49:21 -0300

    atualizando

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,8 +1,11 @@
 git clone https://github.com/FelippeCremona/dotfiles.git ~/dotfiles
+cd ~/dotfiles
 cp ~/.bashrc .
 cp ~/.tmux.conf .
 cp -r ~/.tmuxinator .
 cp ~/.config/starship.toml .
+cp ~/update_dotfiles.sh .
+cp ~/restore_dotfiles.sh .
 git add .
 git commit -m "atualizando"
 git push
