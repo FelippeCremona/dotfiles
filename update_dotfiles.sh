d370a14c3161807254dd76c7727e6b43f3931890
Cremona <felippe.mendes@qintess.com>
2023-03-18 22:00:40 -0300

    atualizando

--- update_dotfiles.sh
+++ update_dotfiles.sh
@@ -1,4 +1,4 @@
-git clone https://github.com/FelippeCremona/dotfiles.git ~/dotfiles
+test -d "/home/cremona/dotfiles" && echo "git clone https://github.com/FelippeCremona/dotfiles.git ~/dotfiles"
 cd ~/dotfiles
 cp ~/.bashrc .
 cp ~/.tmux.conf .
@@ -9,4 +9,3 @@ cp ~/restore_dotfiles.sh .
 git add .
 git commit -m "atualizando"
 git push
-rm -rf ~/dotfiles
