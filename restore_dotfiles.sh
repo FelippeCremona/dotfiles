9418d79610b952d7cb0745f35b24868733cc09ab
Felippe Cremona <felippe.mendes@qintess.com>
2024-10-23 23:12:12 -0300

    atualizando

--- restore_dotfiles.sh
+++ restore_dotfiles.sh
@@ -2,5 +2,6 @@ cp .bashrc ~
 cp .tmux.conf ~
 cp -r .tmuxinator ~
 cp starship.toml ~/.config/
+cp wezterm.lua /mnt/c/Arquivos\ de\ Programas/WezTerm/
 cp update_dotfiles.sh ~
 cp restore_dotfiles.sh ~
