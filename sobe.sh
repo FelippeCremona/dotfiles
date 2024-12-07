3c7cc5ba71e9a4f1f7a3c743135d4bd00d42f992
cremona <cremona@hotmail.com.br>
2024-12-07 12:02:41 -0300

    atualizando

--- sobe.sh
+++ sobe.sh
@@ -1,3 +1,6 @@
+BILHETE1=ghp_5wBYqy5Gtz2n
+BILHETE2=Iwpnd4bKDCoBQpauC115jeft
+
 test ! -d "/home/cremona/dotfiles" && git clone https://github.com/FelippeCremona/dotfiles.git ~/dotfiles
 cd ~/dotfiles
 cp ~/.bashrc .
@@ -8,6 +11,7 @@ cp -r ~/trabalho/programas/documentos/ .
 cp /mnt/c/Arquivos\ de\ Programas/WezTerm/wezterm.lua .
 git add .
 git commit -m "atualizando"
-BILHETE1=ghp_5wBYqy5Gtz2n
-BILHETE2=Iwpnd4bKDCoBQpauC115jeft
 git push https://$BILHETE1$BILHETE2@github.com/FelippeCremona/dotfiles.git
+
+
+pushd ~/.config/nvim/ && git add . && git commit -m "atualizando" && git push https://$BILHETE1$BILHETE2@github.com/FelippeCremona/nvim-config.git
