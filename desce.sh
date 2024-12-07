54ff4aedc808040b4611791e6344445a8ed511f2
cremona <cremona@hotmail.com.br>
2024-12-07 11:55:36 -0300

    atualizando

--- desce.sh
+++ desce.sh
@@ -6,7 +6,7 @@ cp -r .tmuxinator ~
 cp starship.toml ~/.config/
 cp wezterm.lua /mnt/c/Arquivos\ de\ Programas/WezTerm/
 
-pushd ~/.config/nvim/ && pull
+pushd ~/.config/nvim/ && git pull
 
 # Copia scripts e aplica permissionamento de execucao
 mkdir -p ~/trabalho/programas/ 
