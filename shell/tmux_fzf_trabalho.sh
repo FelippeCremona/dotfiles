e74570f74fc060dc202d066248d29a1de24d19ed
Felippe Cremona <f759705@corp.caixa.gov.br>
2025-07-19 16:00:13 -0300

    atualizando

--- shell/tmux_fzf_trabalho.sh
+++ shell/tmux_fzf_trabalho.sh
@@ -4,6 +4,9 @@ find \
   ~/trabalho/caixa/sistemas/sinaf/des/ \
   ~/trabalho/caixa/sistemas/simcn/des/ \
   ~/trabalho/caixa/sistemas/sicvr/des/ \
+  ~/trabalho/caixa/servidores/jboss-eap-7.1-simcn/standalone/configuration/standalone.xml \
+  ~/trabalho/caixa/servidores/jboss-eap-7.1-sinaf/standalone/configuration/standalone.xml \
+  ~/trabalho/caixa/servidores/jboss-eap-7.1-sinaf-acoes-lote/standalone/configuration/standalone.xml \
   -type d -name '.*' -prune -false -o \
   -type d -name 'node_modules' -prune -false -o \
   -type d -name 'target' -prune -false -o \
